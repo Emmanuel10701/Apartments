56f12b24ec6c48d26426263c4b67c7b1b459d591
Emmanuel Makau <emmanuelmakau90@gmail.com>
2024-10-26 15:03:03 +0300

    ✨ Convert MyPage from TSX to JSX and make it client-side compatible! �

---
+++ app/homapage/[id]/page.jsx
@@ -0,0 +1,191 @@
+"use client";
+
+import { useParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { FaStar, FaMapMarkerAlt } from 'react-icons/fa';
+import CircularProgress from '@mui/material/CircularProgress';
+import EmailModal from "../../components/emailModal";
+
+const ApartmentDetail = () => {
+    const { id } = useParams();
+    const [apartment, setApartment] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [mainImage, setMainImage] = useState('');
+    const [imageIndex, setImageIndex] = useState(0);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [apartmentsData, setApartmentsData] = useState([]);
+
+    const fetchApartmentData = async () => {
+        const response = await fetch('/data.json');
+        const data = await response.json();
+        return data;
+    };
+
+    useEffect(() => {
+        const getApartment = async () => {
+            const data = await fetchApartmentData();
+            setApartmentsData(data); // Store all apartments data
+            if (id) {
+                const apartmentFound = data.find((apt) => apt.id.toString() === id);
+                if (apartmentFound) {
+                    setApartment(apartmentFound);
+                    setMainImage(apartmentFound.images[0] || '');
+                } else {
+                    console.error('Apartment not found');
+                }
+            }
+            setLoading(false);
+        };
+
+        getApartment();
+    }, [id]);
+
+    const handleImageClick = (index) => {
+        if (apartment) {
+            setMainImage(apartment.images[index]);
+            setImageIndex(index);
+        }
+    };
+
+    const openEmailModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const closeEmailModal = () => {
+        setIsModalOpen(false);
+    };
+
+    if (loading) {
+        return (
+            <div className="flex justify-center items-center h-screen">
+                <CircularProgress />
+            </div>
+        );
+    }
+
+    if (!apartment) {
+        return (
+            <div className="flex items-center justify-center h-full">
+                <div className="text-3xl font-bold bg-gradient-to-r from-red-500 to-yellow-500 bg-clip-text text-transparent">
+                    No apartment found.
+                </div>
+            </div>
+        );
+    }
+
+    return (
+        <div className="max-w-full mx-auto p-4">
+            <h1 className="text-4xl font-bold bg-gradient-to-r from-purple-500 via-blue-500 to-blue-600 bg-clip-text text-transparent">
+                {apartment.title}
+            </h1>
+            <div className="relative flex-col gap-2">
+                <div className="md:flex flex-col gap-3 p-4 md:gap-6 md:p-8">
+                    <Image
+                        src={mainImage}
+                        alt={apartment.title}
+                        width={800}
+                        height={500}
+                        className="rounded-lg cursor-pointer mt-4 md:w-full md:h-[500px]"
+                    />
+                    <div className="flex flex-wrap p-3 gap-3 md:gap-6 md:p-8">
+                        {apartment.images.map((image, index) => (
+                            index !== imageIndex && (
+                                <div className="flex-1" key={index}>
+                                    <Image
+                                        src={image}
+                                        alt={apartment.title}
+                                        width={400}
+                                        height={300}
+                                        className="rounded-lg cursor-pointer w-full h-full object-cover"
+                                        onClick={() => handleImageClick(index)}
+                                    />
+                                </div>
+                            )
+                        ))}
+                    </div>
+                </div>
+            </div>
+
+            <div className='md:w-[70%] mx-auto w-full shadow-lg rounded-lg'>
+                <div className="p-4 bg-white">
+                    <div className="flex items-center mt-2">
+                        <FaMapMarkerAlt className="text-red-500" />
+                        <span className="ml-2 text-lg font-semibold text-gray-800">{apartment.location}</span>
+                    </div>
+
+                    <div className="flex items-center mt-2">
+                        <div className="flex items-center">
+                            {[...Array(5)].map((_, index) => (
+                                <FaStar key={index} className={`w-4 h-4 ${index < apartment.rating ? 'text-yellow-300' : 'text-gray-200'}`} />
+                            ))}
+                        </div>
+                        <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded ml-3">{apartment.rating}</span>
+                    </div>
+
+                    <p>
+                        <span className="text-blue-800 text-md font-semibold px-2.5 py-0.5 ml-3">{apartment.description}</span>
+                        <p>
+                            <p>Modern Accommodation for Students and Young Professionals</p>
+                            Welcome to your new home—a vibrant and stylish accommodation designed to meet the needs of students and young professionals alike. Our space offers a unique blend of comfort and community, providing an ideal environment for study, work, and socializing.
+                        </p>
+                    </p>
+
+                    <div className="flex items-center justify-between mt-4">
+                        <span className="text-4xl font-bold text-green-600">${apartment.price}/month</span>
+                        <span className="text-sm text-gray-500">Min Price: <span className="text-gray-800 font-semibold">${apartment.minPrice}</span></span>
+                    </div>
+
+                    <div className="flex justify-center mt-4">
+                        <button
+                            className="bg-transparent border border-blue-600 text-blue-600 font-semibold py-2 px-4 rounded-full hover:bg-blue-600 hover:text-white transition duration-300"
+                            onClick={() => window.open(`tel:${apartment.phoneNumber}`)}
+                        >
+                            Call
+                        </button>
+                        <button
+                            className="bg-transparent border border-blue-600 text-blue-600 font-semibold py-2 px-4 rounded-full hover:bg-blue-600 hover:text-white transition duration-300 ml-4"
+                            onClick={openEmailModal}
+                        >
+                            Email
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            {/* Email Modal */}
+            <EmailModal isOpen={isModalOpen} onClose={closeEmailModal} apartment={apartment} />
+
+            {/* Related apartments section */}
+            <h2 className="text-4xl text-center my-4 font-bold mt-6 bg-gradient-to-r from-red-500 via-orange-500 to-yellow-400 text-transparent bg-clip-text">
+                Related Apartments
+            </h2>
+            <div className="flex w-full md:w-2/3 flex-row gap-6 mx-auto overflow-x-auto">
+                {apartmentsData.slice(0, 4).map((relatedApartment) => {
+                    if (relatedApartment.id === apartment.id) return null;
+
+                    return (
+                        <Link href={`/homepage/${relatedApartment.id}`} key={relatedApartment.id}>
+                            <div className="bg-white rounded-lg shadow-md p-4 flex flex-col justify-between h-72 cursor-pointer hover:shadow-lg transition-shadow duration-300">
+                                <Image
+                                    src={relatedApartment.images[0] || ''}
+                                    alt={relatedApartment.title}
+                                    width={400}
+                                    height={300}
+                                    className="rounded-lg h-40 object-cover"
+                                />
+                                <h3 className="font-bold text-md mt-2 bg-gradient-to-r from-blue-500 to-green-500 bg-clip-text text-transparent">
+                                    {relatedApartment.title}
+                                </h3>
+                                <p className="font-bold text-lg text-gray-800">${relatedApartment.price}/month</p>
+                            </div>
+                        </Link>
+                    );
+                })}
+            </div>
+        </div>
+    );
+};
+
+export default ApartmentDetail;
---
+++ app/homapage/page.jsx
@@ -0,0 +1,180 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import Footer from "../components/Footer/page"; 
+import SearchNavbar from "../components/filters"; 
+import ApartmentCard from "../components/card"; 
+import Carousel from "../components/courusel/page"; 
+import CircularProgress from '@mui/material/CircularProgress';
+
+const MainComponent = () => {
+    const [apartmentsData, setApartmentsData] = useState([]);
+    const [filteredApartments, setFilteredApartments] = useState([]);
+    const [currentPage, setCurrentPage] = useState(0);
+    const itemsPerPage = 6; 
+    const [searchTerm, setSearchTerm] = useState('');
+    const [location, setLocation] = useState('');
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchApartmentsData = async () => {
+            const response = await fetch('/data.json');
+            const data = await response.json();
+            setApartmentsData(data);
+            setFilteredApartments(data); // Initialize with full data
+            setIsLoading(false);
+        };
+        fetchApartmentsData();
+    }, []);
+
+    const handleSearch = (filters) => {
+        setIsLoading(true);
+        let filtered = apartmentsData;
+
+        // Apply search filters
+        if (filters.search) {
+            const lowercasedSearch = filters.search.toLowerCase();
+            filtered = filtered.filter(
+                (apartment) =>
+                    apartment.title.toLowerCase().includes(lowercasedSearch) ||
+                    apartment.location.toLowerCase().includes(lowercasedSearch) ||
+                    apartment.rentalType.toLowerCase().includes(lowercasedSearch)
+            );
+        }
+
+        if (filters.location) {
+            const lowercasedLocation = filters.location.toLowerCase();
+            filtered = filtered.filter(apartment =>
+                apartment.location.toLowerCase().includes(lowercasedLocation)
+            );
+        }
+
+        if (filters.minRent !== undefined) {
+            filtered = filtered.filter(apartment => apartment.price >= (filters.minRent || 0));
+        }
+
+        if (filters.maxRent !== undefined) {
+            filtered = filtered.filter(apartment => apartment.price <= (filters.maxRent || Infinity));
+        }
+
+        if (filters.rentalType) {
+            filtered = filtered.filter(apartment => apartment.rentalType === filters.rentalType);
+        }
+
+        setFilteredApartments(filtered);
+        setCurrentPage(0);
+        setTimeout(() => setIsLoading(false), 100); // Delay for loading state
+    };
+
+    const handleSearchSubmit = () => {
+        handleSearch({ search: searchTerm, location });
+    };
+
+    const totalPages = Math.ceil(filteredApartments.length / itemsPerPage);
+    const handlePageChange = (page) => {
+        setCurrentPage(page);
+    };
+
+    const displayedApartments = filteredApartments.slice(
+        currentPage * itemsPerPage,
+        (currentPage + 1) * itemsPerPage
+    );
+
+    return (
+        <div className="flex flex-col p-4">
+            <Carousel />
+            <div className="flex flex-col sm:flex-row items-center justify-evenly mt-20 mx-20 mb-4">
+                <input
+                    type="text"
+                    placeholder="Search apartments..."
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    onFocus={(e) => e.target.classList.add("shadow-md")}
+                    onBlur={(e) => e.target.classList.remove("shadow-md")}
+                    className="w-full sm:w-1/2 p-2 border rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500 transition-shadow duration-300"
+                    aria-label="Search apartments"
+                />
+                <input
+                    type="text"
+                    placeholder="Location..."
+                    value={location}
+                    onChange={(e) => setLocation(e.target.value)}
+                    onFocus={(e) => e.target.classList.add("shadow-md")}
+                    onBlur={(e) => e.target.classList.remove("shadow-md")}
+                    className="w-full sm:w-1/2 p-2 border rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500 mt-2 sm:mt-0 sm:ml-2 transition-shadow duration-300"
+                    aria-label="Search location"
+                />
+                <button
+                    onClick={() => {
+                        handleSearchSubmit();
+                        setSearchTerm('');
+                        setLocation('');
+                    }}
+                    className="mt-2 sm:mt-0 bg-transparent border border-blue-600 text-blue-600 px-4 py-2 rounded-full hover:bg-blue-600 hover:text-white transition duration-300 shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                >
+                    Search
+                </button>
+            </div>
+
+            <SearchNavbar onSearch={handleSearch} />
+
+            <div className="flex items-center flex-col">
+                <h1 className="text-6xl font-bold mt-[5%] text-center bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 bg-clip-text text-transparent">
+                    Available Apartments
+                </h1>
+                <p className="text-center mt-6 mb-20 w-[70%] text-slate-500 text-md">
+                    Browse our collection of apartments and find your ideal home. 
+                    Whether you're looking for a cozy studio or a spacious multi-bedroom, 
+                    we offer a variety of options to suit every lifestyle and budget.
+                </p>
+                {isLoading ? (
+                    <div className="flex justify-center mt-4">
+                        <CircularProgress />
+                    </div>
+                ) : (
+                    <div className="flex flex-wrap justify-between space-y-4">
+                        {displayedApartments.length === 0 ? (
+                            <p className="text-center text-slate-700 text-lg my-10">No apartments found matching your search criteria.</p>
+                        ) : (
+                            displayedApartments.map((apartment) => (
+                                <div className="w-full sm:w-1/2 lg:w-1/3 p-2" key={apartment.id}>
+                                    <ApartmentCard apartment={apartment} />
+                                </div>
+                            ))
+                        )}
+                    </div>
+                )}
+                {totalPages > 1 && (
+                    <div className="flex justify-center mb-20 mt-4 space-x-2">
+                        <button
+                            onClick={() => handlePageChange(currentPage > 0 ? currentPage - 1 : 0)}
+                            disabled={currentPage === 0}
+                            className={`px-4 py-2 rounded-full border border-blue-600 text-blue-600 hover:bg-blue-600 hover:text-white transition duration-300 ${currentPage === 0 ? 'opacity-50 cursor-not-allowed' : ''}`}
+                        >
+                            Previous
+                        </button>
+                        {Array.from({ length: totalPages }, (_, index) => (
+                            <button
+                                key={index}
+                                onClick={() => handlePageChange(index)}
+                                className={`px-4 py-2 rounded-full border border-gray-300 transition duration-300 ${currentPage === index ? 'bg-blue-600 text-white' : 'bg-white text-gray-700 hover:bg-gray-200'}`}
+                            >
+                                {index + 1}
+                            </button>
+                        ))}
+                        <button
+                            onClick={() => handlePageChange(currentPage < totalPages - 1 ? currentPage + 1 : totalPages - 1)}
+                            disabled={currentPage >= totalPages - 1}
+                            className={`px-4 py-2 rounded-full border border-blue-600 text-blue-600 hover:bg-blue-600 hover:text-white transition duration-300 ${currentPage >= totalPages - 1 ? 'opacity-50 cursor-not-allowed' : ''}`}
+                        >
+                            Next
+                        </button>
+                    </div>
+                )}
+            </div>
+            <Footer />
+        </div>
+    );
+};
+
+export default MainComponent;
